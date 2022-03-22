574ce2c84c00d63568e300035b4582db9603eb7a
Leonid <leonid.neigel@gmail.comcd test-2 git config --global user.email leonid.neigel@gmail.comgit config --list>
2022-03-22 19:51:40 +1000

    7

--- script.sh
+++ script.sh
@@ -7,6 +7,7 @@ echo "FAMILY= " $FAMILY
 NAME=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.containerDefinitions[].name`
 echo "NAME= " $NAME
 
+
 sed -i "s#BUILD_NUMBER#$IMAGE_TAG#g" task-definition.json
 sed -i "s#REPOSITORY_URI#$REPOSITORY_URI#g" task-definition.json
 sed -i "s#ROLE_ARN#$ROLE_ARN#g" task-definition.json
@@ -18,4 +19,5 @@ aws ecs register-task-definition --cli-input-json file://task-definition.json --
 REVISION=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.revision`
 echo "REVISION= " "${REVISION}"
 aws ecs update-service --cluster "${CLUSTER_NAME}" --service "${SERVICE_NAME}" --task-definition "${TASK_DEFINITION_NAME}":"${REVISION}" --desired-count "${DESIRED_COUNT}"
+
 
